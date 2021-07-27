804e8056a697399307284ba1840a2241aa6021cf
tolentinoel <rckrleyn@gmail.com>
2021-07-27 16:05:24 -0700

    Adjust img and div size

--- random-hex/src/components/FlipCard.js
+++ random-hex/src/components/FlipCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/FlipCard.css';
 
 class FlipCard extends Component {
     render() {
@@ -6,7 +7,7 @@ class FlipCard extends Component {
             <div className="container">
                 <div className="flipCard">
                     <div className="front"> <h4>FRONT</h4></div>
-                    <div className="back"><img src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" /></div>
+                    <div className="back"><img id="jester" src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" /></div>
                 </div>
             </div>
         );
