e3a7b5537ec9fef1d7da37516d2a37aa7fb79440
tolentinoel <rckrleyn@gmail.com>
2021-07-27 16:23:47 -0700

    Add onClick function to console log and update CSS

--- random-hex/src/components/FlipCard.js
+++ random-hex/src/components/FlipCard.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import '../styles/FlipCard.css';
 
 class FlipCard extends Component {
+    
+    handleClick = (e) => {
+        console.log(e.target)
+    }
+
     render() {
         return (
-            <div className="container">
+            <div className="container" onClick={(e) => this.handleClick(e)}>
                 <div className="flipCard">
                     <div className="front"> <h4>FRONT</h4></div>
                     <div className="back"><img id="jester" src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" /></div>
