a1013565a4f7ffef5f21539aced921af98951f15
tolentinoel <rckrleyn@gmail.com>
2021-07-27 15:27:11 -0700

    Update CSS for FlipCard elements

--- random-hex/src/components/FlipCard.js
+++ random-hex/src/components/FlipCard.js
@@ -8,7 +8,6 @@ class FlipCard extends Component {
                     <div className="front"> <h4>FRONT</h4></div>
                     <div className="back"><img src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" /></div>
                 </div>
-
             </div>
         );
     }
