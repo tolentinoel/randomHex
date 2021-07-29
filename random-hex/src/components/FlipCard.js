ef16881afc3df055b7747d31de0e916e643c0471
tolentinoel <rckrleyn@gmail.com>
2021-07-28 17:09:23 -0700

    Add another card, update handleClick

--- random-hex/src/components/FlipCard.js
+++ random-hex/src/components/FlipCard.js
@@ -4,22 +4,34 @@ import '../styles/FlipCard.css';
 class FlipCard extends Component {
 
     handleClick = (e) => {
-        console.log(e.target) 
+
+    let container = e.target.parentNode.parentNode.className
+    if (container === "container"  ) {
+        document.querySelector('.container').classList.toggle('flip')
+        } else if (container === "container2") {
+            document.querySelector('.container2').classList.toggle('flip')
+        } else if (container === "flipCard") {
+            e.target.parentNode.parentNode.parentNode.classList.toggle('flip')
+        }
     }
 
     render() {
         return (
-            <div className="container" onClick={(e) => this.handleClick(e)}>
-                <div className="flipCard">
-                    <div className="front"> <h4>FRONT</h4></div>
-                    <div className="back"><img id="jester" src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" /></div>
+            <>
+                <div className="container" onClick={(e) => this.handleClick(e)}>
+                    <div className="flipCard">
+                        <div className="front"> <h4>FRONT</h4></div>
+                        <div className="back"><img id="jester" src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" width="400" /></div>
+                    </div>
                 </div>
-                <br/>
-                <div className="flipCard">
-                    <div className="front"> <h4>FRONT</h4></div>
-                    <div className="back"><img id="jester" src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" /></div>
+
+                <div className="container2" onClick={(e) => this.handleClick(e)}>
+                    <div className="flipCard">
+                        <div className="front"> <h4>FRONT</h4></div>
+                        <div className="back"><img id="jester" src="https://cdn1.iconfinder.com/data/icons/photo-stickers-hats/128/hat_12-1024.png" alt="jester hat" width="400" /></div>
+                    </div>
                 </div>
-            </div>
+            </>
         );
     }
 }
